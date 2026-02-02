c5facf44afc9ba7789e9872eeca8ed2e398dffea
IcodeAlpha <alphacode4@outlook.com>
2026-02-02 23:11:46 +0300

    debug: landing debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,8 @@ export default function LandingPage() {
 
       <footer className="bg-card">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 text-center text-foreground/60">
-          <p>&copy; {new Date().getFullYear()} Sipha. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} 
+            . All rights reserved.</p>
         </div>
       </footer>
     </div>
