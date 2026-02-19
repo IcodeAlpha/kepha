20497d3b0841bd7c293c19ed9fa18a60a5bf56f0
IcodeAlpha <alphacode4@outlook.com>
2026-02-19 11:27:44 +0300

    fix: firebase project config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4233703273-8046d",
-  "appId": "1:203890014312:web:b9a0dfd08aa80fd830a3dd",
-  "apiKey": "AIzaSyDreSbmlR4lV5W_4gcEhGvLopzMBTEZVA0",
-  "authDomain": "studio-4233703273-8046d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "203890014312"
+  apiKey: "AIzaSyBp0JPd5PI7kejBX9lXAJycnRbLRvbgO4o",
+  authDomain: "cerviguard.firebaseapp.com",
+  projectId: "cerviguard",
+  storageBucket: "cerviguard.firebasestorage.app",
+  messagingSenderId: "851989673709",
+  appId: "1:851989673709:web:e0b9caf0ac090a63453ee4"
 };
